5463ed93b32de8f27b844ba16715158c9c696f23
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-08-19 19:22:14 +0200

    Making the transition to the AdminArea

--- IvarsSykkelsjappe/IvarsSykkelsjappe/Areas/Admin/Controllers/BikesController.cs
+++ IvarsSykkelsjappe/IvarsSykkelsjappe/Areas/Admin/Controllers/BikesController.cs
@@ -1,4 +1,5 @@
-﻿using IvarsSykkelsjappe.Models.Bikes;
+﻿using AutoMapper;
+using IvarsSykkelsjappe.Models.Bikes;
 using IvarsSykkelsjappe.Services.Bikes;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,10 +8,12 @@ namespace IvarsSykkelsjappe.Areas.Admin.Controllers
     public class BikesController : AdminController
     {
         private readonly IBikeService bikeService;
+        private readonly IMapper mapper;
 
-        public BikesController(IBikeService bikeService)
+        public BikesController(IBikeService bikeService, IMapper mapper)
         {
             this.bikeService = bikeService;
+            this.mapper = mapper;
         }
 
         public IActionResult Index()
@@ -22,6 +25,82 @@ namespace IvarsSykkelsjappe.Areas.Admin.Controllers
         {
             var bikes = this.bikeService.GetAll();
             return View(bikes);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (!User.IsInRole("Admin"))
+            {
+                return Unauthorized();
+            }
+
+            var bike = this.bikeService.GetEdit(id);
+
+            var bikeForm = this.mapper.Map<BikeFormModel>(bike);
+
+            bikeForm.Categories = this.bikeService.GetBikeCategories();
+
+            return View(bikeForm);
+
+            //without AutoMapper
+            //return View(new BikeFormModel
+            //{
+            //    Brand = bike.Brand,
+            //    Model = bike.Model,
+            //    Description = bike.Description,
+            //    Price = bike.Price,
+            //    ImageUrl = bike.ImageUrl,
+            //    Year = bike.Year,
+            //    BikeCategoryId = bike.BikeCategoryId,
+            //    Categories = this.bikeService.GetBikeCategories()
+            //});
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BikeFormModel bikeModel, int id)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                bikeModel.Categories = this.bikeService.GetBikeCategories();
+                return View(bikeModel);
+            }
+
+            this.bikeService.Edit(bikeModel, id);
+            return RedirectToAction(nameof(All));
+        }
+
+        public IActionResult Create()
+        {
+            return View(new BikeFormModel
+            {
+                Categories = this.bikeService.GetBikeCategories()
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Create(BikeFormModel bikeForm)
+        {
+            if (!ModelState.IsValid)
+            {
+                bikeForm.Categories = this.bikeService.GetBikeCategories();
+                return View(bikeForm);
+            }
+
+            this.bikeService.Add(bikeForm);
+            return RedirectToAction(nameof(All));
+        }
+
+        public IActionResult Details(int id)
+        {
+            var bike = this.bikeService.GetDetails(id);
+            return View(bike);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.bikeService.Delete(id);
+            return RedirectToAction(nameof(All));
         }
     }
 }
--- IvarsSykkelsjappe/IvarsSykkelsjappe/Controllers/BikesController.cs
+++ IvarsSykkelsjappe/IvarsSykkelsjappe/Controllers/BikesController.cs
@@ -17,107 +17,107 @@ namespace IvarsSykkelsjappe.Controllers
             this.mapper = mapper;
         }
 
-        [Authorize]
-        [Authorize(Roles = "Admin")]
-        public IActionResult AllAdmin()
-        {
-            var bikes = this.bikeService.GetAll();
-            return View(bikes);
-        }
+        //[Authorize]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult AllAdmin()
+        //{
+        //    var bikes = this.bikeService.GetAll();
+        //    return View(bikes);
+        //}
 
-        [Authorize]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Edit(int id)
-        {
-            if (!User.IsInRole("Admin"))
-            {
-                return Unauthorized();
-            }
+        //[Authorize]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult Edit(int id)
+        //{
+        //    if (!User.IsInRole("Admin"))
+        //    {
+        //        return Unauthorized();
+        //    }
 
-            var bike = this.bikeService.GetEdit(id);
+        //    var bike = this.bikeService.GetEdit(id);
 
-            var bikeForm = this.mapper.Map<BikeFormModel>(bike);
+        //    var bikeForm = this.mapper.Map<BikeFormModel>(bike);
 
-            bikeForm.Categories = this.bikeService.GetBikeCategories();
+        //    bikeForm.Categories = this.bikeService.GetBikeCategories();
 
-            return View(bikeForm);
+        //    return View(bikeForm);
 
-            //without AutoMapper
-            //return View(new BikeFormModel
-            //{
-            //    Brand = bike.Brand,
-            //    Model = bike.Model,
-            //    Description = bike.Description,
-            //    Price = bike.Price,
-            //    ImageUrl = bike.ImageUrl,
-            //    Year = bike.Year,
-            //    BikeCategoryId = bike.BikeCategoryId,
-            //    Categories = this.bikeService.GetBikeCategories()
-            //});
-        }
+        //    //without AutoMapper
+        //    //return View(new BikeFormModel
+        //    //{
+        //    //    Brand = bike.Brand,
+        //    //    Model = bike.Model,
+        //    //    Description = bike.Description,
+        //    //    Price = bike.Price,
+        //    //    ImageUrl = bike.ImageUrl,
+        //    //    Year = bike.Year,
+        //    //    BikeCategoryId = bike.BikeCategoryId,
+        //    //    Categories = this.bikeService.GetBikeCategories()
+        //    //});
+        //}
 
-        [Authorize]
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Edit(BikeFormModel bikeModel ,int id)
-        {
+        //[Authorize]
+        //[HttpPost]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult Edit(BikeFormModel bikeModel ,int id)
+        //{
 
-            if (!ModelState.IsValid)
-            {
-                bikeModel.Categories = this.bikeService.GetBikeCategories();
-                return View(bikeModel);
-            }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        bikeModel.Categories = this.bikeService.GetBikeCategories();
+        //        return View(bikeModel);
+        //    }
 
-            this.bikeService.Edit(bikeModel, id);
-            return RedirectToAction(nameof(AllCustomer));
-        }
+        //    this.bikeService.Edit(bikeModel, id);
+        //    return RedirectToAction(nameof(AllCustomer));
+        //}
 
-        [Authorize]
+        [Authorize(Roles = "Admin, User")]
         public IActionResult AllCustomer(AllBikesQueryModel queryModel)
         {
             this.bikeService.AllSearch(queryModel);
             return View(queryModel);
         }
 
-        [Authorize]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Create()
-        {
-            return View(new BikeFormModel
-            {
-                Categories = this.bikeService.GetBikeCategories()
-            });
-        }
+        //[Authorize]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult Create()
+        //{
+        //    return View(new BikeFormModel
+        //    {
+        //        Categories = this.bikeService.GetBikeCategories()
+        //    });
+        //}
 
 
-        [HttpPost]
-        [Authorize]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Create(BikeFormModel bikeForm)
-        {
-            if (!ModelState.IsValid)
-            {
-                bikeForm.Categories = this.bikeService.GetBikeCategories();
-                return View(bikeForm);
-            }
+        //[HttpPost]
+        //[Authorize]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult Create(BikeFormModel bikeForm)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        bikeForm.Categories = this.bikeService.GetBikeCategories();
+        //        return View(bikeForm);
+        //    }
 
-            this.bikeService.Add(bikeForm);
-            return RedirectToAction(nameof(AllCustomer));
-        }
+        //    this.bikeService.Add(bikeForm);
+        //    return RedirectToAction(nameof(AllCustomer));
+        //}
 
-        [Authorize]
-        public IActionResult Details(int id)
-        {
-            var bike = this.bikeService.GetDetails(id);
-            return View(bike);
-        }
+        //[Authorize]
+        //public IActionResult Details(int id)
+        //{
+        //    var bike = this.bikeService.GetDetails(id);
+        //    return View(bike);
+        //}
 
-        [Authorize]
-        [Authorize(Roles = "Admin")]
-        public IActionResult Delete(int id)
-        {
-            this.bikeService.Delete(id);
-            return RedirectToAction(nameof(AllAdmin));
-        }
+        //[Authorize]
+        //[Authorize(Roles = "Admin")]
+        //public IActionResult Delete(int id)
+        //{
+        //    this.bikeService.Delete(id);
+        //    return RedirectToAction(nameof(AllAdmin));
+        //}
     }
 }
