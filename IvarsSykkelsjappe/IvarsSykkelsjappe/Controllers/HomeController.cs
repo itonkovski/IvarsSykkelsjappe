d004cd27d5ab47ad16effe09e342c0174e4f83c1
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-01-12 21:09:15 +0100

    Adding meta data to the layout view

--- IvarsSykkelsjappe/IvarsSykkelsjappe/Controllers/HomeController.cs
+++ IvarsSykkelsjappe/IvarsSykkelsjappe/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace IvarsSykkelsjappe.Controllers
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
