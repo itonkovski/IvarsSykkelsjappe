ea34925c5f8194078d071b82fbf86cfe4f5b7d44
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-01-13 11:30:50 +0100

    Adding some GlobalConstants and using them in the PrivacyPolicy and the layout

--- IvarsSykkelsjappe/IvarsSykkelsjappe/WebConstants.cs
+++ IvarsSykkelsjappe/IvarsSykkelsjappe/WebConstants.cs
@@ -3,5 +3,11 @@
     public class WebConstants
     {
         public const string GlobalMessageKey = "GlobalMessage";
+
+        public const string SystemName = "IvarsSykkelsjappe";
+        public const string SystemBaseUrl = "https://ivarssykkelsjappe.com";
+        public const string SystemEmail = "info@ivarssykkelsjappe.no";
+
+        public const string SystemSlogan = " - IvarsSykkelsjappe - Bicycle repair service";
     }
 }
